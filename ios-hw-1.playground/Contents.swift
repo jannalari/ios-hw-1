f615f6708683eb0997d7fcfc854d38910f652524
jannalari <66527740+jannalari@users.noreply.github.com>
2020-06-14 21:43:56 +0300

    done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,61 +5,56 @@
  
  */
 
-let name = "Ahmad"
+let name = "ahmad"
+let friendname = "salem"
 
-name = "Salem"
+var myweight = 60.0
+var friendweight = 80.5
+var myheight = 1.5
 
+var weightsum = myweight + friendweight
+var bmi = myweight * (myheight * myheight)
 
-var myWeight = 60
-var FriendWeight = 80.5
-var height = 1.5
+//person1
+let firstName1 = "janna"
+let lastName1 = "ahmad"
+var email1 = "janna.ahmad@gmail.com"
+var phoneNumber1 = "99758336"
+var age1 = 17
+var country1 = "kuwait"
+var area1 = "rumaithiya"
+var password1 = "1234"
+let isKuwaiti1 = true
 
+//person2
+let firstname2 = "alyaa"
+let lastname2 = "ameer"
+var email2 = "alyaagmail.com"
+var phonenumber2 = 88649303
+var age2 = 16
+var country2 = "usa"
+var area2 = "chicago"
+var password2 = "4567"
+let iskuwaiti2 = false
 
-var weightSum = myWeight + FriendWeight
+//person3
+let firstname3 = "ghadeer"
+let lastname3 = "baqer"
+var email3 = "ghadeer500@hotmail.com"
+var phonenumber3 = 74703556
+var age3 = 17
+var country3 = "usa"
+var area3 = "boston"
+var password3 = "910"
+let iskuwaiti3 = false
 
-
-
-var bmi = myWeight * (height * height)
-
-/*
-- - - -
-# المطلوب الثاني
-قم بتعريف بيانات ل ٣ اشخاص، كل شخص يجب ان يكون لدية المعلومات التالية
-  * الاسم
- * الاسم الاخير
- * الايميل
- * رقم الهاتف
- * العمر
- * البلد
- * منطقة السكن
- * باسوورد
- * كويتي ؟
- 
- */
-
-//Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
-
-//Person 2
-
-
-
-
-
-
-
-
-
-
-//Person 3
+//3
+age1 == 17 && age2 == 16  && age3 == 17
+area1 == "rumaithiya" && area2 == "chicago" && area3 == "boston"
+age1 > 18 && age2 > 18 && age3 > 18
+age1 < 18 && age2 < 18 && age3 < 18
+age1 == 17 && age2 == 17 && age3 == 17
+age1 != 17 && age2 != 17 && age3 != 17
 
 
 
@@ -67,18 +62,3 @@ var isKuwaiti = false
 
 
 
-
-
-/*
-- - - -
-# المطلوب الثالث
-* قم بمقارنة معلومات كل شخص مع الشخص الاخر
- * قارن الاعمار اذا كانت متساوية ام لا
- * قارن منطقة السكن اذا كانت متساوية ام لا
- * قارن اذا عمر الاشخاص اكبر من ١٨
- * قارن اذا عمر الاشخاص اصغر من ١٨
- * قارن اذا عمر الاشخاص يساوي  ١٧
- * قارن اذا عمر الاشخاص لا يساوي  ١٧
-
- 
- */
